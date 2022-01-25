97f676164a09725890c1f72917b300b09d20564f
Artem Bagaev <art.bgv@ya.ru>
2022-01-25 21:25:40 +0700

    initial commit

---
+++ WebSocketsSample/ClientsProcessor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+
+namespace WebSocketsSample
+{
+    public class ClientsProcessor
+    {
+        private static ClientsProcessor instance;
+        private Thread processor;
+        private HashSet<WebSocket> clients;
+
+        private ClientsProcessor() 
+        {
+            processor = new Thread(DoProcess);
+            processor.Start();
+
+            clients = new HashSet<WebSocket>();
+        }
+
+        public static ClientsProcessor GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new ClientsProcessor();
+            }
+
+            return instance;
+        }
+
+        private static async void Log(string log) 
+        {
+            var resStr = String.Concat(DateTime.Now.ToString("hh:mm:ss"), " - ", log);
+            using StreamWriter file = new("WriteLines2.txt", true);
+            await file.WriteLineAsync(resStr);
+        }
+
+        private void DoProcess()
+        {
+            while (true)
+            {
+                SendMessage("hello");
+                Thread.Sleep(3000);
+
+                var updatedClients = new HashSet<WebSocket>();
+                foreach (var client in clients)
+                {
+                    var state = client.State;
+                    if (state == WebSocketState.Open)
+                    {
+                        updatedClients.Add(client);
+                    }
+                }
+                clients = updatedClients;
+                Log(clients.Count + " clients");
+            }
+        }
+
+        public void AddClient(WebSocket client)
+        {
+            clients.Add(client);
+        }
+
+        private async void SendMessage(string message)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            foreach (var client in clients)
+            {
+                await client.SendAsync(new ArraySegment<byte>(buffer, 0, buffer.Length), WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+        }
+
+
+    }
+}
---
+++ WebSocketsSample/Controllers/WebSocketController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocketsSample.Controllers
+{
+    public class WebSocketController : ControllerBase
+    {
+        [HttpGet("/ws")]
+        public async Task Get()
+        {
+            var clientsProcessor = ClientsProcessor.GetInstance();
+            
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                using WebSocket webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                clientsProcessor.AddClient(webSocket);
+                await Echo(webSocket);
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+        }
+
+
+
+        private async Task Echo(WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+    }
+}
